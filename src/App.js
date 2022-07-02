51cbcd17deac9534585e308c3697ce59aab9b4a3
Harshit Dwivedi <77053371+Harshit1109@users.noreply.github.com>
2022-07-02 12:11:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import Greet from './components/Greet';
+// import Greet from './components/Greet';
 import './App.css';
-import Details from './components/Details';
+// import Details from './components/Details';
 // import Hello from './components/Hello';
 // import Welcome from './components/Welcome';
-import Message from './components/Message';
+// import Message from './components/Message';
 import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Greet name="Harshit" />
-      <hr />
-      <Details name="Vinice" number="1" />
-      {/* <hr />
-      <Welcome />
-      <hr />
-      <Hello /> */}
-      <hr />
-      <Message />
-      <hr />
+//       <Greet name="Harshit" />
+//       <hr />
       <Counter />
     </div>
   );
